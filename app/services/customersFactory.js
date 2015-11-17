8a2ce633bfe46cfe5eaaaa2cd3df64cdb908a85b
eduardo704 <eduardo704@gmail.com>
2015-11-17 11:17:00 +0000

    adding new styles and navbar

---
+++ app/services/customersFactory.js
@@ -0,0 +1,30 @@
+(function() {
+    var customersFactory = function($http) {
+    var url="https://routerangulars-eduardo704.c9users.io:8081"
+        var factory = {};
+        
+        factory.getCustomers = function() {
+            return $http.get(url+'/customers');
+        };
+        
+        factory.getCustomer = function(customerId) {
+            return $http.get(url+'/customers/' + customerId);
+        };
+        
+        factory.getOrders = function() {
+            return $http.get(url+'/orders');
+        }
+        
+        factory.deleteCustomer = function(customerId) {
+            return $http.delete(url+'/customers/' + customerId);
+        }
+        
+        return factory;
+    };
+    
+    customersFactory.$inject = ['$http'];
+        
+    angular.module('customersApp').factory('customersFactory', 
+                                           customersFactory);
+                                           
+}());
